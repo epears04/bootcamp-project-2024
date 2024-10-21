8e6dff06f3dbb43531445159830e07aedeaa33c3
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-21 01:41:36 -0700

    feat: added typescript and list of Blogs

---
+++ src/blog.ts
@@ -0,0 +1,20 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+    {
+        title: 'A Nice Weekend',
+        date: 'October 20, 2024',
+        description: 'I had a nice weekend going home and hanging out with Zoey and Leili',
+        image: 'resources/me.JPG',
+        imageAlt: 'Picture of a me surrounded by flowers',
+        slug: 'beautiful-day',
+    },
+    
+]
