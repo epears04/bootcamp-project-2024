96366e94211204695c5dc871733d7f8499e48fa2
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-24 23:53:47 -0700

    fix: style sheet link

--- src/blog.js
+++ src/blog.js
@@ -3,7 +3,7 @@ var blogs = [
         title: 'A Nice Weekend',
         date: 'October 20, 2024',
         description: 'I had a nice weekend going home and hanging out with Zoey and Leili',
-        image: './../resources/batch.jpeg',
+        image: './resources/batch.jpeg',
         imageAlt: 'Picture of an ice cream shop',
         slug: 'beautiful-day',
     },
@@ -11,7 +11,7 @@ var blogs = [
         title: 'A Busy Day',
         date: 'October 22, 2024',
         description: 'Today I had a busy day. I went to class, had an interview, worked, went to the gym, and finished homework.',
-        image: './../resources/studying.jpg',
+        image: './resources/studying.jpg',
         imageAlt: 'birdseye image of someone studying at a coffee shop',
         slug: 'busy-day',
     }
