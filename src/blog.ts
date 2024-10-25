7470d25cb2eb9d3e07dad20a0a0e1a361583adba
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-24 23:41:33 -0700

    fix: fixed image links in js

--- src/blog.ts
+++ src/blog.ts
@@ -12,7 +12,7 @@ const blogs: Blog[] = [
         title: 'A Nice Weekend',
         date: 'October 20, 2024',
         description: 'I had a nice weekend going home and hanging out with Zoey and Leili',
-        image: '/resources/batch.jpeg',
+        image: './resources/batch.jpeg',
         imageAlt: 'Picture of an ice cream shop',
         slug: 'beautiful-day',
     },
@@ -20,7 +20,7 @@ const blogs: Blog[] = [
         title: 'A Busy Day',
         date: 'October 22, 2024',
         description: 'Today I had a busy day. I went to class, had an interview, worked, went to the gym, and finished homework.',
-        image: '/resources/studying.jpg',
+        image: './resources/studying.jpg',
         imageAlt: 'birdseye image of someone studying at a coffee shop',
         slug: 'busy-day',
     }
