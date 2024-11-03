8a8643ff9f2c2526da23bad073bdd54835ba0efd
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-11-03 14:52:00 -0800

    feat: added tailwind styling

--- bootcamp-milestone-2/src/app/components/navbar.tsx
+++ bootcamp-milestone-2/src/app/components/navbar.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import style from "./navbar.module.css";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className={style.navbar} >
-        <nav className="navbar">
-            <h1 className="logo"><a href="index.html">Ellie Pearson</a></h1>
-            <div className="nav-list">
-              <Link href="/">Home</Link>
-              <Link href="/blog">Blog</Link>
-              <Link href="/portfolio">Portfolio</Link>
-              <Link href="/resume">Resume</Link>
-              <Link href="/contact">Contact</Link>
-            </div>
-        </nav>
+    <header className="sticky top-0 w-full z-10 bg-[#3C6373] text-[#D9D7C5]">
+      <nav className="container mx-auto flex justify-between items-center p-4">
+        <h1 className="text-lg font-bold trackig-wide">
+          <Link href="/">Ellie Pearson</Link>
+        </h1>
+        <div className="flex space-x-6 mr-8">
+          <Link href="/" className="text-[#D9D7C5] font-semibold hover:underline">Home</Link>
+          <Link href="/blog" className="text-[#D9D7C5] font-semibold hover:underline">Blog</Link>
+          <Link href="/portfolio" className="text-[#D9D7C5] font-semibold hover:underline">Portfolio</Link>
+          <Link href="/resume" className="text-[#D9D7C5] font-semibold hover:underline">Resume</Link>
+          <Link href="/contact" className="text-[#D9D7C5] font-semibold hover:underline">Contact</Link>
+        </div>
+      </nav>
     </header>
   );
 }
