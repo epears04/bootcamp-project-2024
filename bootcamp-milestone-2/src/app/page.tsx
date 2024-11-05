7c98aa5160371f219ef8baa668866996b449035d
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-11-04 18:54:58 -0800

    feat: added basic homepage tsx

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,6 +1,23 @@
+import React from "react";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="about-container">
+      <h1 className="index-title">ABOUT ME</h1>
+       <div className="about-text">
+            <p>Hello! My name is Ellie, nice to meet you</p>
+            <p>
+                I am a second year at Cal Poly SLO majoring in computer science
+                and passionate about <b>bridging the gap between people and technology</b>.
+                I have been fortunate enough to do so through volunteering, work, and research.
+            </p>
+            <p>
+                In my free time I enjoy baking, watching youtube, and working out.
+            </p>
+        </div>
+        <div className="about-image">
+            <img src="me.JPG" alt="picture of me surrounded by flowers" />
+        </div>  
+    </div>
   );
 }
