d0436cc1f1171bd3544ab7a8cd3a683544b06095
epears04 <epears04@users.noreply.github.com>
2024-11-08 01:43:46 +0000

    deploy: bbb0b6e20d34c31ed6375d73c697ea0966f3d52a

--- bootcamp-milestone-2/next.config.ts
+++
@@ -1,9 +0,0 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/bootcamp-milestone-2',
-  trailingSlash: true,
-};
-
-export default nextConfig;
