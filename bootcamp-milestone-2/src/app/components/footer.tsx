7c951cae30523f5e7dad81c962fa82cf94d8968c
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-11-08 19:06:50 -0800

    feat: added footer

---
+++ bootcamp-milestone-2/src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-center p-2 mb-0 bg-header-blue " >
+        <footer className="text-md text-cream">© 2024 Ellie Pearson | All Rights Reserved</footer>
+    </div>
+  );
+}
--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@components/navbar";
+import Footer from "@components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Navbar/>
-        {children}
+          <main>
+             {children}
+          </main>
+          <Footer/>
       </body>
     </html>
   );
