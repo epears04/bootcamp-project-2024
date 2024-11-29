f4c529addee31636c1ad14ed4f8aa872005f3cf4
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-11-29 15:47:42 -0800

    feat: fetch blogs from Mongo

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import BlogPreview from "@components/blogPreview";
-import blogs from "@/static/blogData";
+import Blog from "@database/blogSchema";
+import connectDB from "@database/db";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  async function getBlogs(){
+    await connectDB() // function from db.ts before
+    try {
+        // query for all blogs and sort by date
+        const blogs = await Blog.find().sort({ date: -1 }).orFail()
+        // send a response as the blogs as the message
+        return JSON.parse(JSON.stringify(blogs));
+    } catch (err) {
+        return null
+    }
+  }
+  
+  const blogs = await getBlogs();
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-2 bg-torq">
       <h1 className="p-2 text-2xl font-semibold text-gray-800">BLOG</h1>
       <p className="mb-8 text-lg text-gray-700">Welcome to my blog! Here are some of my latest posts:</p>
-
       <div className="w-full max-w-3xl pb-5 mb-5 space-y-4">
-        {blogs.map((blog) => (
-          <BlogPreview key={blog.slug} {...blog} />
-        ))}
+        {blogs ? (
+          blogs.map((blog: Blog) => (
+            <BlogPreview key={blog.slug} {...blog} />
+          ))
+        ) : (
+          <p className="text-red-500">No blogs found or an error occured</p>
+        )}
       </div>
     </div>
   );
--- bootcamp-milestone-2/src/app/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/app/components/blogPreview.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import type { Blog } from "@/static/blogData";
+import type Blog from "@database/blogSchema";
 
 export default function BlogPreview(props: Blog) {
   return (
     <Link href={`/blog/${props.slug}`} passHref>
       <div className="flex flex-col items-center max-w-2xl p-6 mx-auto mb-8 rounded-lg shadow-lg cursor-pointer bg-cream">
         <h3 className="mb-2 text-2xl font-bold text-gray-800">{props.title}</h3>
-        <p className="text-gray-700">Posted on: {props.date}</p>
-        <Image
-          src={props.image}
-          alt={props.imageAlt}
-          width={500}
-          height={300}
-          className="rounded-lg"
-        />
+        <p className="text-gray-700">Posted on: {new Date(props.date).toLocaleDateString()}</p>
+        {props.image &&(
+          <Image
+            src={props.image}
+            alt={props.imageAlt}
+            width={500}
+            height={300}
+            className="rounded-lg"
+          />
+        )}
         <p className="p-2 m-2 text-center text-gray-800">{props.description}</p>
+        <p className="text-center text-gray-700">{props.content}</p>
       </div>
     </Link>
   );
